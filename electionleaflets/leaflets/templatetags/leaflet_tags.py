3d70994f07c14415d3bfcdf77dfc9ec6b482b881
Ross Jones <rossdjones@gmail.com>
2011-04-21 21:00:18 +0100

    Some tweaks for 2011 season

--- electionleaflets/leaflets/templatetags/leaflet_tags.py
+++ electionleaflets/leaflets/templatetags/leaflet_tags.py
@@ -25,26 +25,16 @@ def truncatesmart(value, limit=80):
         {{ string|truncatesmart }}
         {{ string|truncatesmart:50 }}
     """
-    
     try:
         limit = int(limit)
-    # invalid literal for int()
     except ValueError:
-        # Fail silently.
         return value
     
-    # Make sure it's unicode
     value = unicode(value)
     
-    # Return the string itself if length is smaller or equal to the limit
     if len(value) <= limit:
         return value
     
-    # Cut the string
     value = value[:limit]
-    
-    # Break into words and remove the last
     words = value.split(' ')[:-1]
-    
-    # Join the words and return
     return ' '.join(words) + '...'
--- electionleaflets/parties/templatetags/party_tags.py
+++ electionleaflets/parties/templatetags/party_tags.py
@@ -8,6 +8,8 @@ register = template.Library()
 @register.inclusion_tag('parties/ordered_list.html')
 def party_list_by_count():
     from parties.models import Party
-    
+    from datetime import datetime
+    when = datetime(year=2010,month=5, day=1)
     parties = Party.objects.order_by('-count').all()[0:10]
+    
     return { 'MEDIA_URL': settings.MEDIA_URL, 'parties': parties }

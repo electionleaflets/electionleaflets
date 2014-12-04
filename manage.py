78f70359563a3f3f81124811749526cfc6fe50cb
Sym Roe <sym.roe@talusdesign.co.uk>
2014-12-04 09:32:44 +0000

    Stop using buildout's manage.py
    Part of move away from buildout

--- manage.py
+++ manage.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
+import os
+import sys
 
-import djangorecipe.manage
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "electionleaflets.settings")
 
-if __name__ == '__main__':
-    djangorecipe.manage.main('electionleaflets.settings_prod')
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)

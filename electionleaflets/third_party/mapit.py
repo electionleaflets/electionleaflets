3fd60a8a0f4b996a850de4658634d7dbb2d1b25f
Anna Powell-Smith <annapowellsmith@gmail.com>
2014-12-06 15:28:01 +0000

    Remove explicit simplejson import
    Django 1.5+ drops support for Python 2.5 so we can assume that json is
    available: https://docs.djangoproject.com/en/dev/releases/1.5/#django-utils-simplejson

--- electionleaflets/third_party/mapit.py
+++ electionleaflets/third_party/mapit.py
@@ -4,8 +4,7 @@ Performs a lookup of postcode to Lat/Lon using the mapit.mysociety.org API.
 
 def postcode_to_latlong(postcode):
     import urllib2
-    from django.utils import simplejson as json
-    
+
     url = 'http://mapit.mysociety.org/postcode/%s' % postcode.lower().replace(' ', '')
     result_dict = None
     try:
@@ -16,5 +15,5 @@ def postcode_to_latlong(postcode):
 
     if not result_dict or 'error' in result_dict:
         return None, None
-        
+
     return result_dict['wgs84_lat'], result_dict['wgs84_lon']

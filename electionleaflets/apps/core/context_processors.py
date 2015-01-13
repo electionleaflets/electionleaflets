13a94cff87de0b0f7cefe07bb615968038cf29e9
Sym Roe <sym.roe@talusdesign.co.uk>
2015-01-13 17:29:11 +0000

    White spaces

--- electionleaflets/apps/core/context_processors.py
+++ electionleaflets/apps/core/context_processors.py
@@ -4,9 +4,9 @@ def settings(request):
     """
     Add settings (or some) to the templates
     """
-    from django.conf         import settings
+    from django.conf import settings
 
-    tags = {}            
+    tags = {}
     tags['GOOGLE_MAPS_KEY'] = settings.GOOGLE_MAPS_KEY
     tags['MAP_PROVIDER'] = settings.MAP_PROVIDER
     if hasattr(settings, 'GOOGLE_ANALYTICS_KEY'):

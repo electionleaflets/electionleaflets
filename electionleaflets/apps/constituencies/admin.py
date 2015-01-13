f406adba16ec98a724c33d2acf3c86de2ea15c95
Sym Roe <sym.roe@talusdesign.co.uk>
2015-01-13 17:13:59 +0000

    Initial migration away from M2M constituencies

--- electionleaflets/apps/constituencies/admin.py
+++ electionleaflets/apps/constituencies/admin.py
@@ -1,19 +1,14 @@
 from django.contrib     import admin
-from constituencies.models import Constituency, ConstituencyType
+from constituencies.models import Constituency
 
 class ConstituencyOptions(admin.ModelAdmin):
-    list_display         = [ 'name', 'slug','constituency_type']    
-    search_fields        = ['name']  
+    list_display         = [ 'name', 'slug', ]
+    search_fields        = ['name']
     ordering             = ['name']
-    
+
 class ConstituencyTypeOptions(admin.ModelAdmin):
-    list_display         = [ 'name', 'country']    
-    search_fields        = ['name']  
+    list_display         = [ 'name', 'country']
+    search_fields        = ['name']
 
-    
+
 admin.site.register( Constituency, ConstituencyOptions )
-admin.site.register( ConstituencyType, ConstituencyTypeOptions )
-
-
-
-
--- electionleaflets/apps/constituencies/views.py
+++ electionleaflets/apps/constituencies/views.py
@@ -1,34 +1,12 @@
+import json
+
 from django.template  import RequestContext
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.conf import settings
 
-def constituency_by_postcode(postcode):
-    import urllib2
-    from xml.etree import ElementTree
-    
-    key = settings.THEYWORKFORYOU_API_KEY
-    if not key:
-        return None
-        
-    url = "http://www.theyworkforyou.com/api/getConstituency?output=xml&key=%s&postcode=%s" % (key,postcode.replace(' ',''),)
-    try:
-        conn = urllib2.urlopen( url )        
-        data = conn.read()
-        
-        element = ElementTree.fromstring(data)
-        err = element.find('error')
-        if err is None:
-            elem = element.find('name')
-            if elem is not None:
-                return elem.text
-    except:
-        pass
-        
-    return None
-    
-    
+
 def view_not_spots(request):
     from constituencies.models import Constituency
     constituencies = Constituency.objects.filter(count=0).order_by('name').all()
@@ -37,22 +15,22 @@ def view_not_spots(request):
                                 'constituencies': constituencies,
                             },
                             context_instance=RequestContext(request) )
-    
+
 
 def view_constituencies(request):
     """
-    Provides a form to lookup constituencies either by name or by 
+    Provides a form to lookup constituencies either by name or by
     postcode.
     """
     from constituencies.models import Constituency
     from constituencies.forms import ConstituencyLookupForm
     from leaflets.models import Leaflet
-    
+
     constituencies = Constituency.objects.order_by('name').all()
-    
+
     form = ConstituencyLookupForm(request.POST or None)
     cons = None
-    
+
     if request.method == 'POST':
         if form.is_valid():
             postcode = form.cleaned_data['search']
@@ -62,12 +40,12 @@ def view_constituencies(request):
                     cons = Constituency.objects.get(name=c)
             else:
                 cons = form.cleaned_data['constituency']
-                
+
             if cons:
                 return HttpResponseRedirect(reverse('constituency', kwargs={'slug': cons.slug}) )
 
-    
-    return render_to_response('constituencies/index.html', 
+
+    return render_to_response('constituencies/index.html',
                             {
                                 'constituencies': constituencies,
                             },
@@ -78,17 +56,17 @@ def view_constituency(request, slug):
     from constituencies.models import Constituency
     from leaflets.models import Leaflet
     import math
-    
+
     const = get_object_or_404(Constituency, slug=slug)
-    
-    qs = Leaflet.objects.filter(constituencies__id=const.id).order_by('-id')
-    
+
+    qs = Leaflet.objects.filter(constituency=const.pk).order_by('-pk')
+
     total = qs.count()
     currentPage = request.GET.get('page', 1)
     totalPages = int(math.ceil(float(total)/12))
-    
-    
-    return render_to_response('constituencies/constituency.html', 
+
+
+    return render_to_response('constituencies/constituency.html',
                             {
                                 'constituency': const,
                                 'qs': qs,
